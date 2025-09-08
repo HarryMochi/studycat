04ff779f4fb90bf0e7236fbb9665f92b7bbe54fa
David <minhdavid.truong@gmail.com>
2025-09-08 17:15:08 +0200

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,17 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="44c3a21a-d2ed-40c1-9f3b-15ba275cb562"></script>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HP0G6MC9Q9"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HP0G6MC9Q9');
+          `}
+        </script>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <AuthProvider>
