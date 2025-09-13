b9cbd242b30781f4c66a8afa0c9e7c071122f36b
David <minhdavid.truong@gmail.com>
2025-09-13 14:47:45 +0200

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
